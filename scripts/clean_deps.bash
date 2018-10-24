dd2d8fd6b72940276f365fb35fcc608b7edca207
aaron <aaronchongth@gmail.com>
2018-10-24 02:13:28 -0400

    added clean 3rd party

--- scripts/clean_deps.bash
+++ scripts/clean_deps.bash
@@ -4,4 +4,5 @@ echo ">[CLEAN_DEPS]<START>< Cleaning built artifacts and built dependencies..."
 rm -rf build_files
 rm -rf bin
 rm -rf deps/opencv/build/
+rm -rf 3rd_party/*
 echo ">[CLEAN_DEPS]<DONE>< Cleaning built artifacts and built dependencies..."
